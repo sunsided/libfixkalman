a710b464131117c3560186a994debe5f73383489
Markus Mayer <development@widemeadows.de>
2014-01-16 02:41:13 +0100

    some Kalman optimizations

--- main.c
+++ main.c
@@ -37,7 +37,51 @@ void test_matrix_inverse()
     assert(testf >= 1.3);
 }
 
+/**
+* \brief Tests matrix inversion using Cholesky decomposition
+*/
+void test_matrix_abat()
+{
+    int result;
+
+    // data buffer for the original and decomposed matrix
+    mf16 a;
+    a.errors = 0;
+    a.columns = a.rows = 3;
+    mf16_fill_diagonal(&a, fix16_one);
+    a.data[0][1] = F16(0.5);
+    a.data[0][2] = F16(0.5);
+    a.data[1][0] = F16(0.5);
+
+    // data buffer for the inverted matrix
+    mf16 b;
+    b.errors = 0;
+    b.columns = b.rows = 3;
+    b.data[0][0] = F16(1);
+    b.data[0][1] = F16(2);
+    b.data[0][2] = F16(3);
+    b.data[1][0] = F16(4);
+    b.data[1][1] = F16(5);
+    b.data[1][2] = F16(6);
+    b.data[2][0] = F16(7);
+    b.data[2][1] = F16(8);
+    b.data[2][2] = F16(9);
+
+    mf16 m;
+    m.errors = 0;
+    m.columns = m.rows = 3;
+    
+    mf16_mul(&b, &a, &b);
+    mf16_mul_bt(&m, &b, &a);
+
+    fix16_t test = m.data[0][0];
+    float testf = fix16_to_float(test);
+
+    assert(testf == 16);
+}
+
 void main() 
 {
     test_matrix_inverse();
+    test_matrix_abat();
 }
