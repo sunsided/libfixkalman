f00f6d7a6f3c6ba39e0f78b2e3f635ba73b30bbf
Markus Mayer <development@widemeadows.de>
2014-01-17 15:27:11 +0100

    removed mf16_invert_lt (was merged into libfixmatrix)

--- fixmatrixadditions.h
+++ fixmatrixadditions.h
@@ -4,15 +4,6 @@
 #include "fixmatrix.h"
 
 /*!
-* \brief Inverts a lower triangular square matrix
-* \param[out] dest The destination matrix
-* \param[in] matrix The lower triangular square matrix to invert
-*
-* Kudos: https://code.google.com/p/efficient-java-matrix-library
-*/
-void mf16_invert_lt(mf16 *dest, const mf16 *matrix);
-
-/*!
 * \brief Calculates A*B*A'
 */
 void mf16_mul_abat(mf16 *dest, const mf16 *a, const mf16 *b);
