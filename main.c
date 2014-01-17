58747486de18414168d38d3f71fb95a5f1b82c37
Markus Mayer <development@widemeadows.de>
2014-01-17 15:28:01 +0100

    corrected the unit test

--- main.c
+++ main.c
@@ -8,22 +8,15 @@
 */
 void test_matrix_inverse()
 {
-    int result;
-
-    // data buffer for the original and decomposed matrix
-    mf16 d;
-    d.columns = d.rows = 3;
+    // the original matrix
+    mf16 d = { 3, 3, 0, 0 };
     mf16_fill_diagonal(&d, fix16_one);
     d.data[0][1] = F16(0.5);
     d.data[1][0] = F16(0.5);
-    
-    // data buffer for the inverted matrix
-    mf16 di;
-    di.columns = di.rows = 3;
-    mf16_fill(&di, 0);
-    
-    mf16 m;
-    m.columns = m.rows = 3;
+
+    // the decomposed and inverted matrix 
+    mf16 m = { 3, 3, 0, 0 };
+    mf16_fill(&m, 0);
 
     // decompose matrix to lower triangular
     mf16_cholesky(&m, &d);
@@ -34,7 +27,7 @@ void test_matrix_inverse()
     // test the result
     fix16_t test = m.data[1][1];
     float testf = fix16_to_float(test);
-    assert(testf >= 1.3);
+    assert(testf >= 1.3 && testf <= 1.4);
 }
 
 /**
@@ -42,21 +35,15 @@ void test_matrix_inverse()
 */
 void test_matrix_abat()
 {
-    int result;
-
     // data buffer for the original and decomposed matrix
-    mf16 a;
-    a.errors = 0;
-    a.columns = a.rows = 3;
+    mf16 a = { 3, 3, 0 };
     mf16_fill_diagonal(&a, fix16_one);
     a.data[0][1] = F16(0.5);
     a.data[0][2] = F16(0.5);
     a.data[1][0] = F16(0.5);
 
     // data buffer for the inverted matrix
-    mf16 b;
-    b.errors = 0;
-    b.columns = b.rows = 3;
+    mf16 b = { 3, 3, 0 };
     b.data[0][0] = F16(1);
     b.data[0][1] = F16(2);
     b.data[0][2] = F16(3);
@@ -67,9 +54,7 @@ void test_matrix_abat()
     b.data[2][1] = F16(8);
     b.data[2][2] = F16(9);
 
-    mf16 m;
-    m.errors = 0;
-    m.columns = m.rows = 3;
+    mf16 m = { 3, 3, 0 };
     
     mf16_mul(&b, &a, &b);
     mf16_mul_bt(&m, &b, &a);
