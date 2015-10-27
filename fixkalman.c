a1b4e90e4455999b25bf6c7ab5079edffb284c0e
bdvd <david.bodnar@st.ovgu.de>
2015-10-27 16:30:36 +0100

    KALMAN_TIME_VARYING replaced with standard kalman filter with control input

--- fixkalman.c
+++ fixkalman.c
@@ -310,10 +310,21 @@ void kalman_filter_initialize(kalman16_t *const kf, uint_fast8_t num_states, uin
     kf->B.errors = 0;
     mf16_fill(&kf->B, 0);
 
+#ifndef KALMAN_TIME_VARYING
+
+    kf->Q.rows = num_states;
+    kf->Q.columns = num_states;
+    kf->Q.errors = 0;
+    mf16_fill(&kf->Q, 0);
+
+#else
+
     kf->Q.rows = num_inputs;
     kf->Q.columns = num_inputs;
     kf->Q.errors = 0;
     mf16_fill(&kf->Q, 0);
+
+#endif
 
     kf->x.rows = num_states;
     kf->x.columns = 1;
@@ -487,7 +498,8 @@ void kalman_predict_P(register kalman16_t *const kf)
 
     /************************************************************************/
     /* Predict next covariance using system dynamics and input              */
-    /* P = A*P*A' + B*Q*B'                                                  */
+    /* P = A*P*A' + B*Q*B' if KALMAN_TIME_VARYING is defined                */
+    /* P = A*P*A' + Q if KALMAN_TIME_VARYING is not defined                 */
     /************************************************************************/
 
     // P = A*P*A'
@@ -499,7 +511,11 @@ void kalman_predict_P(register kalman16_t *const kf)
     // P = P + B*Q*B'
     if (B->rows > 0)
     {
+#ifndef KALMAN_TIME_VARYING
+        mf16_add(P, P, Q);                      // P += Q
+#else
         mf16_mul_abat_add(P, B, Q);             // P += B*Q*B'
+#endif
     }
 }
 
@@ -521,7 +537,7 @@ void kalman_predict_P_uc(register kalman16_uc_t *const kf)
 
     /************************************************************************/
     /* Predict next covariance using system dynamics and input              */
-    /* P = A*P*A' + B*Q*B'                                                  */
+    /* P = A*P*A' + Q                                                       */
     /************************************************************************/
 
     // P = A*P*A'
@@ -544,7 +560,7 @@ void kalman_cpredict_P_uc(register kalman16_uc_t *const kf, register fix16_t del
 
     /************************************************************************/
     /* Predict next covariance using system dynamics and input              */
-    /* P = A*P*A' + B*Q*B'                                                  */
+    /* P = (A*P*A' + Q) * deltaT                                            */
     /************************************************************************/
 
     // P = A*P*A'
@@ -590,7 +606,8 @@ void kalman_predict_P_tuned(register kalman16_t *const kf, fix16_t lambda)
 
     /************************************************************************/
     /* Predict next covariance using system dynamics and input              */
-    /* P = A*P*A' * 1/lambda^2 + B*Q*B'                                     */
+    /* P = A*P*A' * 1/lambda^2 + B*Q*B' if KALMAN_TIME_VARYING is defined   */
+    /* P = A*P*A' * 1/lambda^2 + Q if KALMAN_TIME_VARYING is not defined    */
     /************************************************************************/
 
     // P = A*P*A'
@@ -602,7 +619,11 @@ void kalman_predict_P_tuned(register kalman16_t *const kf, fix16_t lambda)
     // P = P + B*Q*B'
     if (B->rows > 0)
     {
-        mf16_mul_abat_add(P, B, &kf->Q);     // temp = B*Q*B'
+#ifndef KALMAN_TIME_VARYING
+        mf16_add(P, P, &kf->Q);                 // P += Q
+#else
+        mf16_mul_abat_add(P, B, &kf->Q);        // temp = B*Q*B'
+#endif
     }
 }
 
@@ -642,7 +663,7 @@ void kalman_predict_P_tuned_uc(register kalman16_uc_t *const kf, fix16_t lambda)
 
     /************************************************************************/
     /* Predict next covariance using system dynamics and input              */
-    /* P = A*P*A' * 1/lambda^2 + B*Q*B'                                     */
+    /* P = A*P*A' * 1/lambda^2 + Q                                          */
     /************************************************************************/
 
     // P = A*P*A'
